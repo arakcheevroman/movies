3fa5520a108141f940c1c0dd621f8ca027ea15f1
arakcheevroman <arakceev@ls23l32jfff2lejjf2f2.local>
2018-04-08 00:52:21 +0300

    change test component

--- src/components/test.js
+++ src/components/test.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Test extends Component {
     render() {
-        return <div>Тестовый</div>
+        return <div>Тестовый 3</div>
     }
 
 }
