70d5170f8a35429c664b8f443a6df3f03cfe3cb5
arakcheevroman <arakcheevroman@gmail.com>
2018-04-08 01:13:25 +0300

    change test component 2

--- src/components/test.js
+++ src/components/test.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Test extends Component {
     render() {
-        return <div>Тестовый 3</div>
+        return <div>Тестовый</div>
     }
 
 }
