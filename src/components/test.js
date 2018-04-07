f22b27b6710b4400087b24a9048c61e5a6a5f437
arakcheevroman <arakcheevroman@gmail.com>
2018-04-08 01:35:59 +0300

    change text in file

--- src/components/test.js
+++ src/components/test.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Test extends Component {
     render() {
-        return <div>Тестов123ы6й</div>
+        return <div>test</div>
     }
 
 }
